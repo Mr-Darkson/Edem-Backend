63db1fc0c6fca77c8e6fc4407f7cdaba8f975581
Aleksey <tryaskov.alex@yandex.ru>
2024-03-10 20:42:34 +0300

    lexa 2 commit

--- src/main/java/com/coursework/edem/EdemBackend/controllers/EdemController.java
+++ src/main/java/com/coursework/edem/EdemBackend/controllers/EdemController.java
@@ -1,17 +1,23 @@
 package com.coursework.edem.EdemBackend.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
-@RequestMapping()
 public class EdemController {
 
-    @GetMapping()
+    @GetMapping("/")
     public String homePage() {
         return "home/main";
     }
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        //model.addAttribute("title", "Про нас:");
+        return "home/registration";
+    }
+
 
 
 }
