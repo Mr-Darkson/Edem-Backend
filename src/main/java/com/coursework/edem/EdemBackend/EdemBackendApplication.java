d1dbcb89e5e01372f3b82b83bc246e52b3aca146
EchoTerror <najikore@mail.ru>
2024-03-10 19:29:10 +0300

    Initial commit

---
+++ src/main/java/com/coursework/edem/EdemBackend/EdemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coursework.edem.EdemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coursework/edem/EdemBackend/EdemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coursework.edem.EdemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
