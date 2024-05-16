957b7913d6be575c8688765e13b791431a37ed64
EchoTerror <najikore@mail.ru>
2024-05-16 23:54:41 +0300

    Lazy update frondend

--- src/main/java/com/coursework/edem/EdemBackend/repositories/MessageRepository.java
+++ src/main/java/com/coursework/edem/EdemBackend/repositories/MessageRepository.java
@@ -5,8 +5,7 @@ import com.coursework.edem.EdemBackend.models.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Integer>  {
-
 }
