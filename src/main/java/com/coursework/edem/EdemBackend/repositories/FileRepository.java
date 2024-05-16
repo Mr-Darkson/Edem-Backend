2e9129e6491172033ad47043c76b36af70f3f64b
Aleksey <tryaskov.alex@yandex.ru>
2024-05-16 18:31:43 +0300

    corrections

--- src/main/java/com/coursework/edem/EdemBackend/repositories/FileRepository.java
+++ src/main/java/com/coursework/edem/EdemBackend/repositories/FileRepository.java
@@ -4,6 +4,9 @@ import com.coursework.edem.EdemBackend.models.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
+    Optional<File> findByFilename(String filename);
 }
--- src/main/java/com/coursework/edem/EdemBackend/services/FileUploadService.java
+++ src/main/java/com/coursework/edem/EdemBackend/services/FileUploadService.java
@@ -7,8 +7,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.UUID;
+
 
 @Service
 @AllArgsConstructor
@@ -19,7 +19,10 @@ public class FileUploadService {
         String filePath = "C://Users/User/Desktop/edem/Edem-Backend/src/main/data/";
         // String filePath = "C://edem/Edem-Backend/src/main/data/"; // версия для вас
         for (int i=0;i<multipartFile.length;i++){
-            String fileName = multipartFile[i].getOriginalFilename();
+            String fileName = UUID.randomUUID().toString() + multipartFile[i].getOriginalFilename();
+            while (fileRepository.findByFilename(fileName).isPresent()) {
+                fileName = UUID.randomUUID().toString() + multipartFile[i].getOriginalFilename();
+            }
             try {
                 FileUtil.uploadFile(multipartFile[i].getBytes(), filePath, fileName);
                 fileRepository.save(new File(messageId, fileName));
