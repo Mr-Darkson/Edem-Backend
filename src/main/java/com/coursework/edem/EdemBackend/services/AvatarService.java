60bd58f47ccafdb87cd6eb24cb6f42310d867d0b
NKulbaka <nikita.kulbaka@mail.ru>
2024-06-16 13:26:49 +0300

    Fixed avatars in target

--- src/main/java/com/coursework/edem/EdemBackend/services/AvatarService.java
+++ src/main/java/com/coursework/edem/EdemBackend/services/AvatarService.java
@@ -17,16 +17,32 @@ public class AvatarService {
 
     @Autowired
     private PersonRepository personRepository;
+    // для intellij
+//    public File getAvatarByPersonId(Long id) {
+//        Person person = personRepository.findById(id).orElse(null);
+//        Avatar avatar = avatarRepository.findAvatarByPerson(person).orElse(null);
+//
+//        String filePath = System.getProperty("user.dir") + "/src/main/";
+//        if (avatar == null) {
+//            filePath = filePath + "resources/static/img/sms/Ellipse.png";
+//        } else {
+//            filePath = filePath + "avatars/" + avatar.getAvatarName();
+//        }
+//
+//        return new File(filePath);
+//    }
 
+    // для ручного запуска
     public File getAvatarByPersonId(Long id) {
         Person person = personRepository.findById(id).orElse(null);
         Avatar avatar = avatarRepository.findAvatarByPerson(person).orElse(null);
 
-        String filePath = System.getProperty("user.dir") + "/src/main/";
+        String filePath;
         if (avatar == null) {
-            filePath = filePath + "resources/static/img/sms/Ellipse.png";
+            filePath = System.getProperty("user.dir") + "/classes/static/img/sms/Ellipse.png";
+            System.out.println(filePath);
         } else {
-            filePath = filePath + "avatars/" + avatar.getAvatarName();
+            filePath = System.getProperty("user.dir") + "/src/main/avatars/" + avatar.getAvatarName();
         }
 
         return new File(filePath);
